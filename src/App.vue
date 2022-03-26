4872b620a34e7df2ef647f94ab50f8dae63a96ce
berke <bab26@st-andrews.ac.uk>
2022-03-26 23:29:15 +0300

    Added a timer which starts counting after block appears, and stops when clicked on it

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>Reaction Timer</h1>
-  <button @click="start">play</button>
-  <Block v-if="isPlaying" />
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay ="delay"/>
 </template>
 
 <script>
--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,12 +1,35 @@
 <template>
-  <div class="block">
+  <div class="block" v-if="showBlock" @click="stopTimer">
       click me
   </div>
 </template>
 
 <script>
 export default {
-
+    props: ['delay'],
+    data() {
+        return {
+            showBlock: false,
+            timer: null,
+            reactionTime: 0
+        }
+    },
+    mounted() {
+        setTimeout(() => {
+            this.showBlock = true;
+            this.startTimer()
+        }, this.delay)
+    },
+    methods: {
+        startTimer() {
+            this.timer = setInterval(() => {
+                this.reactionTime += 10
+            }, 10)
+        },
+        stopTimer() {
+            clearInterval(this.timer)
+        }
+    }
 }
 </script>
 
